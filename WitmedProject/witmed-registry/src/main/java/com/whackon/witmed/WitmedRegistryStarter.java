441ec3040857d0e1bff50e9dba691c472c70a2e5
wyl397540705 <wangyongle1912@163.com>
2021-12-29 16:48:53 +0800

    创建 Witmed Parent 模块

---
+++ WitmedProject/witmed-registry/src/main/java/com/whackon/witmed/WitmedRegistryStarter.java
@@ -0,0 +1,18 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <b>智慧医疗信息平台 - Eureka 注册中心启动类</b>
+ *
+ * @author mi
+ * @date 2021/12/29
+ * @since
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class WitmedRegistryStarter {
+
+}
